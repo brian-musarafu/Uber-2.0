4e9e30b03a324f04610ceb20dc84222d066d03e2
Brian Musarafu <musarafubrian01@gmail.com>
2023-06-04 00:13:48 +0200

    Added Navigation

---
+++ screens/EatsScreen.js
@@ -0,0 +1,16 @@
+/** @format */
+
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const EatsScreen = () => {
+	return (
+		<View>
+			<Text>EatsScreen</Text>
+		</View>
+	);
+};
+
+export default EatsScreen;
+
+const styles = StyleSheet.create({});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,25 @@
 /** @format */
 
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet, Image } from 'react-native';
 import React from 'react';
 import tw from 'tailwind-react-native-classnames';
+import NavOptions from '../components/NavOptions';
 
 const HomeScreen = () => {
 	return (
-		<SafeAreaView>
-			<Text style={tw`text-red-500 p-10`}>HomeScreen</Text>
+		<SafeAreaView style={tw`bg-white h-full`}>
+			<View style={tw`p-5`}>
+				<Image
+					style={{
+						width: 100,
+						height: 100,
+						resizeMode: 'contain',
+					}}
+					source={{ uri: 'https://links.papareact.com/gzs' }}
+				/>
+
+				<NavOptions />
+			</View>
 		</SafeAreaView>
 	);
 };
