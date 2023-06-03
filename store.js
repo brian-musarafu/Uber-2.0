35872c21be25af3e679d816a3f681c43bc7f379f
Brian Musarafu <musarafubrian01@gmail.com>
2023-06-03 21:42:38 +0200

    Added Redux

---
+++ store.js
@@ -0,0 +1,10 @@
+/** @format */
+
+import { configureStore } from '@reduxjs/toolkit';
+import navReducer from './slices/navSlice';
+
+export const store = configureStore({
+	reducer: {
+		nav: navReducer,
+	},
+});
