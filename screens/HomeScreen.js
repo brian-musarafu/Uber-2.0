974beb324a77f5b6d7c9dc8d42b2aa04edc78503
Brian Musarafu <musarafubrian01@gmail.com>
2023-06-03 22:07:51 +0200

    Added to screens, HomeScreen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,17 @@
+/** @format */
+
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import React from 'react';
+import tw from 'tailwind-react-native-classnames';
+
+const HomeScreen = () => {
+	return (
+		<SafeAreaView>
+			<Text style={tw`text-red-500 p-10`}>HomeScreen</Text>
+		</SafeAreaView>
+	);
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
